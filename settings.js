643b135bc88430587de3826c4f85214383b27f7b
rongamerp <115156468+rongamerp@users.noreply.github.com>
2022-10-06 18:45:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
+global.owner = ['94763422146']
+global.premium = ['94763422146']
+global.ownernomer = '94763422146'
+global.ownername = 'Dark ron'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
